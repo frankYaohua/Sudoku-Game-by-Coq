3b149cfa352983ca024e556177139ccc9e67c6b3
yaohua <628zhao@gmail.com>
2018-04-25 11:51:07 -0500

    add message and two questions

--- Sudoku_Game_Board.v
+++ Sudoku_Game_Board.v
@@ -4,13 +4,23 @@ Unset Strict Implicit.
 Require Export Sudoku_Engine_Final.
 Require Export Sudoku_Game_Repo.
 
-Goal solvable level_1.
+Goal solvable level_0.
 Proof.
-unfold level_1.
-f_7.
-f_4.
-f_4.
-f_3.
-f_6.
-Save solution.
-Print solution.
+unfold level_0.
+f_1.
+f_8.
+Qed.
+
+Goal solvable level_2.
+Proof.
+unfold level_2.
+f_6. f_5. f_9. f_8.
+f_7. f_4. f_8. f_9. f_3. f_6.
+f_9. f_3. f_7. f_1. f_2.
+f_3. f_6. f_4. f_5. f_9.
+f_1. f_8. f_6. f_2. f_4.
+f_7. f_2. f_1. f_4. f_6. f_8.
+f_2. f_1. f_5. f_7. f_3.
+f_8. f_9. f_2. f_6. f_7. 
+f_9. f_7. f_8. f_4. f_3. f_5. f_2. f_1.
+Qed.
--- Sudoku_Game_Repo.v
+++ Sudoku_Game_Repo.v
@@ -1,3 +1,24 @@
+(*
+
+Name: Sudoku_Game_Repo (a part of Sudoku_Coq, final project of CS386L)
+
+Author: Yaohua Zhao
+
+Date: 2018.04.25
+
+Total Number of Levels: XXX
+
+Last Update: 2018.04.25
+
+Description:
+This is the Sudoku game repo. The difficulity is increasing with increasing of
+the level number. There are total XXX from level_0 to level XXX in the repo.
+Feel free to add you own Sudoku question. But do not forget the cursor symbol "+".
+
+References: 
+
+*)
+
 (* add you own path here, which is absolute path *)
 Add LoadPath "~/coqFinal/Sudoku_Coq".
 
@@ -5,15 +26,42 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Require Export Sudoku_Engine_Final.
 
+(* level_0 is just for tutorial *)
+Definition level_0 :=
+  +> X 2 3 4 5 6 7 8 9 <|
+  |> 4 5 6 7 8 9 1 2 3 <|
+  |> 7 8 9 1 2 3 4 5 6 <|
+  |> 2 3 4 5 6 7 8 9 1 <|
+  |> 5 6 7 8 9 1 2 3 4 <|
+  |> 8 9 1 2 3 4 5 6 7 <|
+  |> 3 4 5 6 7 8 9 1 2 <|
+  |> 6 7 8 9 1 2 3 4 5 <|
+  |> 9 1 2 3 4 5 6 7 _ <|
+  |><|
+  .
+
 Definition level_1 :=
-  |> 5 3 4 6 7 8 9 1 2 <|
-  +> 6 X 2 1 9 5 3 4 8 <|
-  |> 1 9 8 3 4 2 5 6 7 <|
-  |> 8 5 9 7 6 1 4 2 3 <|
-  |> 4 2 6 8 5 3 7 9 1 <|
-  |> 7 1 3 9 2 _ 8 5 6 <|
-  |> 9 6 1 5 3 7 2 8 _ <|
-  |> 2 8 7 4 1 9 6 3 5 <|
-  |> _ 4 5 2 8 _ 1 7 9 <|
+  +> 5 3 _ _ 7 _ _ _ _ <|
+  |> 6 _ _ 1 9 5 _ _ _ <|
+  |> _ 9 8 _ _ _ _ 6 _ <|
+  |> 8 _ _ _ 6 _ _ _ 3 <|
+  |> 4 _ _ 8 _ 3 _ _ 1 <|
+  |> 7 _ _ _ 2 _ _ _ 6 <|
+  |> _ 6 _ _ _ _ 2 8 _ <|
+  |> _ _ _ 4 1 9 _ _ 5 <|
+  |> _ _ _ _ 8 _ _ 7 9 <|
+  |><|
+  .
+
+Definition level_2 :=
+  +> 1 3 X 2 _ _ 7 4 _ <|
+  |> _ 2 5 _ 1 _ _ _ _ <|
+  |> 4 8 _ _ 6 _ _ 5 _ <|
+  |> _ _ _ 7 8 _ 2 1 _ <|
+  |> 5 _ _ _ 9 _ 3 7 _ <|
+  |> 9 _ _ _ 3 _ _ _ 5 <|
+  |> _ 4 _ _ _ 6 8 9 _ <|
+  |> _ 5 3 _ _ 1 4 _ _ <|
+  |> 6 _ _ _ _ _ _ _ _ <|
   |><|
   .
